d9a444bee8f6fa757fd738ea2bd158c2e262e02c
HS THANUSH <hsthanush.bs23@bmsce.ac.in>
2025-09-05 21:40:03 +0530

    Update and rename client to client main.py

---
+++ client main.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+API_URL = "http://127.0.0.1:8000/ride_request/"
+
+
+payload = {
+    "user_id": 1,
+    "source": "Bangalore",
+    "destination": "Mysore"
+}
+
+
+response = requests.post(API_URL, json=payload)
+
+if response.status_code == 200:
+    print(" Ride request created successfully!")
+    print("Response:", json.dumps(response.json(), indent=4))
+else:
+    print(" Failed with status code:", response.status_code)
+    print("Response:", response.text)
+
